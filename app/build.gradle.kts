ba4e4e48136b02820fb52c5300b43d47c2305cf0
mukari <1923579853@qq.com>
2026-07-08 20:34:49 +0800

    upd:基本框架搭建

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,17 @@ android {
     buildFeatures {
         compose = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
